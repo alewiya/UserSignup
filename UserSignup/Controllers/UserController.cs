7a3371a02231aeca1c6bdf806fa4a03869668806
Alewiya <duressaalewiya@gmail.com>
2020-01-06 14:57:46 -0600

    last spot before modifying code

--- UserSignup/Controllers/UserController.cs
+++ UserSignup/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using UserSignup.Models;
+using UserSignup.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,70 +16,37 @@ namespace UserSignup.Controllers
         //public List<User> users = new List<User>();
         //public IActionResult Index()
 
-        public IActionResult Index(string username = "User") {
-            ViewBag.username = username;
-            //ViewBag.user = UserData.GetAll();
+        public IActionResult Index(string userName="User") {
+
+            ViewBag.UserName = userName;
             return View();
+                //ViewBag.user = UserData.GetAll();
+           
         }
         
         public IActionResult Add()
         {
-            return View();
+            AddUserViewModel addUserViewModel = new AddUserViewModel();
+
+            return View(addUserViewModel);
         }
 
         [HttpPost]
-        public IActionResult Add(User user,string verify)
+        public IActionResult Add(AddUserViewModel addUserViewModel)
         {
-            if(user.UserName == null || user.UserName.Length < 5 && user.UserName.Length > 15)
+            if (ModelState.IsValid)
             {
-                ViewBag.error1 = "Please enter valid username which is greater than 5"
-                return View();
+                User user1 = new User()
+                {
+                    UserName = addUserViewModel.UserName,
+                    Email = addUserViewModel.Email,
+                    Password = addUserViewModel.Password
+                };
+                return Redirect("Index?useName=" + user1.UserName);
+
+
             }
-            else if (user.UserName.Any(char.IsDigit))
-            {
-                ViewBag.error2 = "Please enter valid username with out";
-                return View();
-            }
-            else if ( user.Email == null)
-            {
-                return View();
-            }
-
-            if (user.Password != null && user.Password.Equals(verify))
-            {
-
-                
-
-                //return View("Index");
-                //i can use qaury parameter
-                return Redirect("Index?username=" + user.UserName);
-               
-            }
-            else
-            {
-                ViewBag.username = user.UserName;
-                ViewBag.email = user.Email;
-                ViewBag.error3 = "Please enter valid username and password";
-                return View();
-            }
-
-
-            
-             
-             
-            
-           
-                
-
-    
-
-
-
-
-
-
-
-
+            return View(addUserViewModel);
         }
     }
 }
---
+++ UserSignup/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserSignup.ViewModels
+{
+    public class AddUserViewModel
+    {
+      [Required]
+        public string UserName { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(20,MinimumLength =6,ErrorMessage =" It is InValid password")]
+        public string Password { get; set; }
+        [Required]
+        //[Compare("Password",ErrorMessage ="Password don't match")]
+        public int Verfiy { get; set; }
+    public AddUserViewModel() { }
+    }
+}
